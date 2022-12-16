22508daba5f1ae2c63e1d5202df38d68b94aff58
Shani Anconina <shanianconina310@gmail.com>
2022-12-16 12:38:09 +0200

    add lines and switch lines

--- js/memeController.js
+++ js/memeController.js
@@ -17,74 +17,130 @@ function renderMeme() {
         gCanvas.width = this.naturalWidth
         gCanvas.height = this.naturalHeight
         gCtx.drawImage(this, 0, 0)
-        drawText(`${meme.lines[0].txt}`, gCanvas.width / 2, 0)
-        if (meme.lines[meme.lines.length - 1].txt && meme.lines.length !== 1) drawText(`${meme.lines[meme.lines.length - 1].txt}`, gCanvas.width / 2, gCanvas.height - 40)
+        let lineIdx = 0
+        meme.lines.forEach(line => {
+            drawText(line, lineIdx)
+            lineIdx++
+        })
+        // if (meme)
+        // drawText(`${meme.lines[0].txt}`, gCanvas.width / 2, 0)
+        // if (meme.lines[meme.lines.length - 1].txt && meme.lines.length !== 1) drawText(`${meme.lines[meme.lines.length - 1].txt}`, gCanvas.width / 2, gCanvas.height - 40)
     }
     document.querySelector('.editor-screen').classList.remove('hide')
     document.querySelector('.gallery-screen').classList.add('hide')
 }
 
-function onMoveToGalleryScreen(){
+function onMoveToGalleryScreen() {
     document.querySelector('.editor-screen').classList.add('hide')
     document.querySelector('.gallery-screen').classList.remove('hide')
 }
 
-function drawText(text, x, y) {
-    const meme = getMeme()
+function drawText(line, lineIdx) {
     gCtx.lineWidth = 2
-    gCtx.strokeStyle = 'black'
-    gCtx.fillStyle = `${meme.lines[meme.selectedLineIdx].color}`
-    gCtx.font = `${meme.lines[meme.selectedLineIdx].size}px ${meme.lines[meme.selectedLineIdx].family}`;
-    gCtx.textAlign = `${meme.lines[0].align}`
+    gCtx.strokeStyle = line.strokeColor
+    gCtx.fillStyle = line.fillColor
+    gCtx.font = `${line.size}px ${line.family}`;
+    // gCtx.textAlign = 'center'
     gCtx.textBaseline = 'top'
 
-    gCtx.fillText(text, x, y)
-    gCtx.strokeText(text, x, y)
+    let x
+    let y
+
+    // if (lineIdx === 0) y = 0
+    // else if (lineIdx === 1) y = gCanvas.height - 40
+    // else y = gCanvas.height / 2
+    switch (lineIdx) {
+        case 0:
+            y = 0
+            gCtx.textBaseline = 'top'
+            break
+
+        case 1:
+            y = gCanvas.height
+            gCtx.textBaseline = 'bottom'
+            break
+
+        default:
+            y = gCanvas.height / 2
+            gCtx.textBaseline = 'middle'
+            break
+    }
+
+    switch (line.align) {
+        case 'left':
+            x = 5
+            gCtx.textAlign = 'left'
+            break
+
+        case 'center':
+            x = gCanvas.width / 2
+            gCtx.textAlign = 'center'
+            break
+
+        case 'right':
+            x = gCanvas.width - 5
+            gCtx.textAlign = 'right'
+            break
+    }
+
+
+
+    gCtx.fillText(line.txt, x, y)
+    gCtx.strokeText(line.txt, x, y)
 }
+
+function onImgSelect(imgId) {
+    setImg(imgId)
+    gCurrImgId = imgId
+    renderMeme()
+}
+
+// ------------------ CRUD ------------------ //
+
+function onSwitchLine() {
+    switchLine()
+    renderMeme()
+}
+
+function onCreateLine() {
+    createLine()
+    renderMeme()
+}
+
+// ------------------ EDITOR BUTTONS & INPUTS ------------------ //
 
 function onSetLineTxt(lineTxt) {
     setLineTxt(lineTxt)
     renderMeme()
 }
 
-function onImgSelect(imgId){
-    setImg(imgId)
-    gCurrImgId = imgId
-    renderMeme()
-}
-
-function onPickFillColor(color){
-    pickFillColor(color)
-    renderMeme()
-}
-
-// function onPickStrokeColor(color){
-//     pickStrokeColor(color)
-//     renderMeme()
-// }
-
-function onIncreaseFont(){
+function onIncreaseFont() {
     increaseFont()
     renderMeme()
 }
 
-function onDecreaseFont(){
+function onDecreaseFont() {
     decreaseFont()
     renderMeme()
 }
 
-function onSwitchLine(){
-    switchLine()
+function onAlignmentLCR(alignTo) {
+    alignmentLCR(alignTo)
     renderMeme()
 }
 
-function onSetFontFamily(fontFamily){
+function onSetFontFamily(fontFamily) {
     setFontFamily(fontFamily)
     renderMeme()
 }
 
-function onAlignmentLCR(alignTo){
-    alignmentLCR(alignTo)
+function onPickFillColor(fillColor) {
+    pickFillColor(fillColor)
+    renderMeme()
+}
+
+function onPickStrokeColor(strokeColor) {
+    pickStrokeColor(strokeColor)
     renderMeme()
 }
 
--- js/service/memeService.js
+++ js/service/memeService.js
@@ -7,19 +7,13 @@ let gMeme = {
     selectedLineIdx: 0,
     lines: [
         {
-            txt: 'I love pizza',
+            txt: '',
             size: 40,
             align: 'center',
-            color: 'white',
+            fillColor: 'white',
+            strokeColor: 'black',
             family: 'impact'
-        },
-        {
-            txt: 'I love food',
-            size: 30,
-            align: 'center',
-            color: 'white',
-            family: 'impact'
-        },
+        }
     ]
 }
 
@@ -27,21 +21,38 @@ function getMeme() {
     return gMeme
 }
 
-function setLineTxt(lineTxt) {
-    gMeme.lines[gMeme.selectedLineIdx].txt = lineTxt
-}
-
 function setImg(imgId) {
     gMeme.selectedImgId = imgId
 }
 
-function pickFillColor(color) {
-    gMeme.lines[gMeme.selectedLineIdx].color = color
+// ------------------ CRUD ------------------ //
+
+function switchLine() {
+    if (gMeme.selectedLineIdx === gMeme.lines.length - 1) gMeme.selectedLineIdx = 0
+    document.querySelector('.text-editor').value = gMeme.lines[gMeme.selectedLineIdx].txt
+    gMeme.selectedLineIdx++
 }
 
-// function pickStrokeColor(color) {
-//     gMeme.lines[gMeme.selectedLineIdx].color = color
-// }
+function createLine() {
+    const newLine = {
+        txt: '',
+        size: 40,
+        align: 'center',
+        fillColor: 'white',
+        strokeColor: 'black',
+        family: 'impact'
+    }
+    gMeme.lines.push(newLine)
+    document.querySelector('.text-editor').value = ''
+    gMeme.selectedLineIdx++
+    console.log('gMeme.lines:', gMeme.lines)
+}
+
+// ------------------ EDITOR BUTTONS & INPUTS ------------------ //
+
+function setLineTxt(lineTxt) {
+    gMeme.lines[gMeme.selectedLineIdx].txt = lineTxt
+}
 
 function increaseFont() {
     gMeme.lines[gMeme.selectedLineIdx].size += 1
@@ -51,28 +62,18 @@ function decreaseFont() {
     gMeme.lines[gMeme.selectedLineIdx].size -= 1
 }
 
-function switchLine() {
-    gMeme.selectedLineIdx++
-    if (gMeme.selectedLineIdx === gMeme.lines.length - 1) gMeme.selectedLineIdx = 0
-    console.log('selectedLineIdx:', gMeme.selectedLineIdx)
-    console.log('lines.length:', gMeme.lines.length)
-
+function alignmentLCR(alignTo) {
+    gMeme.lines[gMeme.selectedLineIdx].align = alignTo
 }
 
 function setFontFamily(fontFamily) {
     gMeme.lines[gMeme.selectedLineIdx].family = fontFamily
 }
 
-function alignmentLCR(alignTo) {
-    gMeme.lines[gMeme.selectedLineIdx].align = alignTo
+function pickFillColor(fillColor) {
+    gMeme.lines[gMeme.selectedLineIdx].fillColor = fillColor
 }
 
-function _createLine() {
-    return {
-        txt: '',
-        size: 40,
-        align: 'left',
-        color: ''
-    }
-
+function pickStrokeColor(strokeColor) {
+    gMeme.lines[gMeme.selectedLineIdx].strokeColor = strokeColor
 }
